89a931622bebf4ab23db883fad3cbdd0d4f280a6
Vym38224 <jardaavymetal@gmail.com>
2022-03-21 09:35:23 +0100

    skola

--- main.py
+++ main.py
@@ -1,21 +1,12 @@
 #!/usr/bin/env python3
 
-from os.path import basename, splitext
+import pylab as pl
 import tkinter as tk
+
+from pylab import linspace, pi, plot,sin,cos, show,grid,legend
+from os.path import basename, splitext
 from tkinter import *
-import matplotlib.pyplot as plt
-import numpy as np
-from pylab import pi
 
-
-
-class About(tk.Toplevel):
-    def __init__(self, parent):
-        super().__init__(parent, class_=parent.name)
-        self.config()
-
-    def close(self):
-        self.destroy()
 
 
 class Application(tk.Tk):
@@ -30,58 +21,71 @@ class Application(tk.Tk):
         self.lbl.pack()
         self.lbl2 = tk.Label(self, text="Nastavení frekvence:")
         self.lbl2.pack()   
-        self.f = Entry(self)
-        self.f.pack()
-        self.f.focus_set()
+        self.entry = Entry(self)
+        self.entry.pack()
+        self.entry.focus_set()
         def frekvence():
             try:
                 f1 = open("frekvence.txt", "w")
             except FileNotFoundError:
                 print("Soubor nebyl nalezen")
 
-            text = (self.f.get())
+            text = (self.entry.get())
             f1.write(text)
-            print(self.f.get())
+            print(self.entry.get())
             f1.close()
         self.btn3 = tk.Button(self, text="zapiš", command=frekvence)
         self.btn3.pack()
-        self.btn2 = tk.Button(self, text="ukaž graf", command=self.graf)
-        self.btn2.pack()
+        self.btn2 = tk.Button(self, text="ukaž graf", command=self.grafsoubor)
+        self.btn2.pack() 
         self.btn = tk.Button(self, text="Quit", command=self.quit)
         self.btn.pack()
-        self.hodnoty = []
+
+    def quit(self, event=None):
+        super().quit()
+        
 
         
+    def grafsoubor(self):
+        f = open("soubor-win.txt", "r")
+        x = []
+        y = []
+        while 1:
+            radek = f.readline()
+            if radek =="":
+                break
+            cisla = radek.split()
+            x.append(float(cisla[0]))
+            y.append(float(cisla[1]))
+        f.close()
+        pl.figure()
+        pl.plot(x,y)
+        pl.grid(True)
+        pl.show()
+        
+        
+        
+        
+
+
+
     def graf(self):
-        #Čas
-        f2 = open("soubor-win.txt", "r")
-        radky = f2.readlines()
-        x = [float(line.split()[0]) for line in radky]
-        self.hodnoty.append(x)
-        for radek in self.hodnoty:
-                zacatek = radek[0]
-                konec = radek[-1]
-        x = np.linspace(zacatek, konec,len(radky))
-        f2.close      
         #Frekvence
         f1 = open("frekvence.txt", "r")         
         f = f1.read()
         f = np.int16(f)                         
         f1.close()
         #Napětí                           
-        u = np.cos(2*pi*f*x)
+        #u = np.cos(2*pi*f*x)
         #plot     
-        plt.plot(x,u, c="r", linewidth=2)
-        plt.grid()
+        #plt.plot(x,u, c="r", linewidth=2)
+       #plt.grid()
         plt.ylabel("napětí[V]")
         plt.xlabel("čas[s]")
         plt.title("Cosinus")
-        plt.show()
+        #plt.show()
 
         
-    def quit(self, event=None):
-        super().quit()
-
 app = Application()
 app.mainloop()
 
